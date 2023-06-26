bbe2199186a773ec319496d6844f5958778ef40c
Ygor <ygor_fernandes1996@hotmail.com>
2023-06-26 18:18:05 -0300

    Realizando entrega

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,99 @@
 /* Desenvolva sua lógica aqui ... */
+import {products, categories} from './productsData.js'
+
+function createCard(product){
+    const card = document.createElement("li")
+    card.classList.add('card');
+    /* Trabalhe sua lógica aqui */
+    const image = document.createElement('img');
+    image.src = product.img 
+
+    const divCard = document.createElement('div');
+    divCard.classList.add('textBoxCard');
+
+    const yearAndName = document.createElement('p');
+    yearAndName.innerHTML = `${product.band}  ${product.year}`;
+
+    const albums = document.createElement('h2');
+    albums.innerHTML = `${product.title}`;
+
+    const buySpan = document.createElement('span');
+    buySpan.classList.add('buySpan');
+
+    const price = document.createElement('p');
+    price.classList.add('price');
+    price.innerHTML = `R$ ${product.price}.00`;
+
+    const buyButton = document.createElement('button');
+    buyButton.classList.add('buyButton');
+    buyButton.innerText = "Comprar";
+    
+    card.append(image, yearAndName, albums, buySpan, divCard);
+    buySpan.append(price, buyButton);
+    divCard.append(yearAndName, albums, buySpan);
+    
+    return card
+}
+
+const renderCategoryButtons = (categories) => {
+    const listButton = document.querySelector('.listButton__container ul');   
+    
+    categories.forEach((category) => {
+        const list = document.createElement('li');
+        const categoryButton =document.createElement('button');
+        categoryButton.innerHTML = `${category}`;
+        categoryButton.classList.add('button-filter')
+        
+        listButton.appendChild(list);
+        list.appendChild(categoryButton);
+    });
+}
+renderCategoryButtons(categories);
+
+const renderCards = (products) => {
+    const cardContainer = document.querySelector('.card__container ul');
+    cardContainer.innerHTML = "";
+    const inputRange = document.querySelector('input');
+    const TextPrice = document.querySelector('p');
+    TextPrice.innerHTML = `Até R$ ${inputRange.value}`;
+
+    products.forEach((product) => {
+        const card = createCard(product);
+        cardContainer.appendChild(card);
+    })
+}
+renderCards(products);
+
+const eventElements = (categories, products) => {
+    const buttons = document.querySelectorAll('.button-filter');
+    const inputRange = document.querySelector('input');
+    const TextPrice = document.querySelector('p');
+    
+    let categoryIndex = 0;
+    
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            categoryIndex = categories.findIndex(category => category === button.innerHTML);
+            
+            if (categoryIndex == 0) {  
+                renderCards(products);
+            } else {
+                const filteredArray = products.filter(product => product.category == categoryIndex);
+                renderCards(filteredArray);
+            }
+        });
+    });
+    
+    inputRange.addEventListener('input', () => {
+        TextPrice.innerHTML = `Até R$ ${inputRange.value}`;
+        
+        if (categoryIndex == 0) {  
+            const filteredArray = products.filter(product => product.price <= inputRange.value);
+            renderCards(filteredArray);
+        } else {
+            const filteredArray = products.filter(product => product.price <= inputRange.value && product.category == categoryIndex);
+            renderCards(filteredArray);
+        }
+    })
+}
+eventElements(categories, products);
--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,1 +1,38 @@
 /* Desenvolva sua lógica aqui ... */
+const darkMode = () => {
+    const darkModeButton = document.querySelector('.darkModeButton');
+    const body = document.querySelector('body');
+    const Buttons = document.querySelectorAll('button')
+    const cards = document.querySelectorAll('.card');
+    let theme ='dark-mode';
+    
+    darkModeButton.addEventListener('click', () => {
+        body.classList.toggle(theme);
+        Buttons.forEach((button) => {
+            button.classList.toggle(theme);
+        });
+        cards.forEach((card) => {
+           card.classList.toggle(theme);
+        });
+
+    if (body.classList.contains(theme)) {
+        localStorage.setItem("@openMusic:darkMode", "true");
+        darkModeButton.innerHTML = `<i class="fa-solid fa-sun"></i>`;
+    } else {
+        localStorage.setItem("@openMusic:darkMode", "false");
+        darkModeButton.innerHTML =`<i class="fa-solid fa-moon"></i>`;
+    }
+    });
+
+    const localStorageItem = JSON.parse(localStorage.getItem("@openMusic:darkMode"));
+    console.log(localStorageItem);
+
+    if (localStorageItem) { 
+        body.classList.add(theme);
+        darkModeButton.innerHTML = `<i class="fa-solid fa-sun"></i>`;
+    } else {
+        body.classList.remove(theme);
+    }
+
+}
+darkMode();
